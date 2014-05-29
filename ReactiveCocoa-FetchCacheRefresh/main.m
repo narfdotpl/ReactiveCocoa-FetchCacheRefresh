0e6e17088150e89e12d5c19830c3b41d4e5f459a
Maciej Konieczny <narf@macoscope.net>
2014-05-29 19:14:17 +0200

    created repo :cake:

---
+++ ReactiveCocoa-FetchCacheRefresh/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  ReactiveCocoa-FetchCacheRefresh
+//
+//  Created by Maciej Konieczny on 2014-05-29.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+
+    return 0;
+}
+
